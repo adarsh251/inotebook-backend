9cf470b1a12fe203a08c1b46b73b711cd8220e7b
adarsh251 <mishraadarsh25104@gmail.com>
2024-06-26 20:30:22 +0530

    sign jwt on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,9 @@ const {validationResult}=require('express-validator');
 const bcrypt = require('bcrypt');
 const path=require('path');
 const User=require(path.join(__dirname,'..','model','User'));
+require('dotenv').config();
+const jwt=require('jsonwebtoken');
+
 const handleLogin= async (req,res)=>{
     const result=validationResult(req);
     if(result.isEmpty()){
@@ -14,7 +17,18 @@ const handleLogin= async (req,res)=>{
                 const match=await bcrypt.compare(req.body.password,user.password);
                 if(match){
                     //sign token
-                    res.status(200).json(user);
+                    const accessToken=jwt.sign(
+                        {
+                            "email":req.body.email,
+                            "id":user._id,
+                        },
+                        process.env.ACCESS_TOKEN_SECRET,
+                        {
+                            expiresIn:'10m',
+                        }
+                    );
+                    res.cookie(accessToken);
+                    res.status(200).json({accessToken,user});
                 }
                 else{
                     res.status(401).json({"msg":"Incorrect password"});
@@ -22,6 +36,7 @@ const handleLogin= async (req,res)=>{
             }
         }
         catch(err){
+            console.log(err);
             res.status(500).json(err);
         }
     }

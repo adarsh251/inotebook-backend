86d6f3503f54a94a83cf8294218d2edb2194f72b
adarsh251 <mishraadarsh25104@gmail.com>
2024-06-26 13:49:12 +0530

    password hashing during user registration

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,27 +1,36 @@
 const {validationResult}=require('express-validator');
 const path=require('path');
 const User=require(path.join(__dirname,'..','model','User'));
+const bcrypt = require('bcrypt');
 const createUser= async (req,res)=>{
     const result=validationResult(req);
     // const arr=JSON.stringify(result);
     // console.log(arr);
     if(result.isEmpty()){
         const user=await User.findOne({email:req.body.email});
-        if(!user){
-            await User.create({
-                "user":req.body.user,
-                "email":req.body.email,
-                "password":req.body.password
-            })
-            res.status(201).send(`ok ${result}`);
+        if(!user){  
+            try{
+                const salt=await bcrypt.genSalt(10);
+                const hashedpwd=await bcrypt.hash(req.body.password,salt);
+                await User.create({
+                    "user":req.body.user,
+                    "email":req.body.email,
+                    "password":hashedpwd
+                })
+                res.status(201).json({"msg":`New user: ${req.body.user} created`});
+            }
+            catch(err){
+                console.log(err);
+                res.status(500).send(err);
+            }
         }
         else{
-            res.status(404).send("Email already exists");
+            res.status(404).json({"msg":"Email already exists"});
         }
     }
     else{
         console.log(result.errors); 
-        res.status(404).send(result.errors[0].msg);
+        res.status(404).json({"msg":result.errors[0].msg});
     }
 }
 module.exports={createUser};

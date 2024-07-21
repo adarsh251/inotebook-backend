81f824b452f8e98e4ad333f5bbc1c7d230299531
adarsh251 <mishraadarsh25104@gmail.com>
2024-07-22 01:07:56 +0530

    correct cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins=[
-    'https://inotebook-sooty-beta.vercel.app/',
-    'https://inotebook-adarsh-mishras-projects-c53eeb4c.vercel.app/',
-    'https://inotebook-git-master-adarsh-mishras-projects-c53eeb4c.vercel.app/',
+    'https://inotebook-sooty-beta.vercel.app',
+    'https://inotebook-adarsh-mishras-projects-c53eeb4c.vercel.app',
+    'https://inotebook-git-master-adarsh-mishras-projects-c53eeb4c.vercel.app',
     'http://localhost:3000'
 ];
 
